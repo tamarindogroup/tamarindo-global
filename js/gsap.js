c4518d69e58c1c9aae0518af606547b3bd50b075
tamarindogroup <60480059+tamarindogroup@users.noreply.github.com>
2022-11-30 22:39:08 +0000

    build

---
+++ js/gsap.js
@@ -0,0 +1,38 @@
+// let tween = gsap.fromTo(".logos__img-wrapper", 
+//     {
+//         opacity: 0
+//     }, 
+//     {
+//         opacity: 1, 
+//         duration: 1, 
+//         ease: "elastic",
+//         stagger: {
+//             amount: 2,
+//             from: "random"
+//           }
+//     });
+
+
+//     let tween = gsap.fromTo(".logos__img-wrapper", 
+//     {opacity: 0}, 
+//     {opacity: 1, duration: 20, ease: "elastic", stagger: {
+//     amount: 2,
+//     from: "random"
+//   }}
+//     );
+
+
+let logoTween = gsap.to(".logos__img-wrapper", 
+    {   
+        scrollTrigger: ".logos__img-wrapper",
+        opacity: 1, 
+        duration: 2, 
+        ease: "ease",
+        stagger: {
+            amount: 2,
+            from: "random"
+          },
+        // paused: true,
+    });
+
+    
