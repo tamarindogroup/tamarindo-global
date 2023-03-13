65c7040a7e593a57911be2cdff1409663206b718
tamarindogroup <60480059+tamarindogroup@users.noreply.github.com>
2023-03-13 17:14:19 +0000

    Create load.js

---
+++ js/load.js
@@ -0,0 +1,34 @@
+/* loads an element from another collection template page */
+
+/* SETUP
+
+target element has the following attributes:
+data-tam-load-element="load-target"
+data-tam-load-collection="COLLECTION SLUG"
+data-tam-load-item="ITEM SLUG"
+data-tam-load-id="IDENTIFIER"
+
+source element has the following attributes:
+data-tam-load-element="load-source"
+data-tam-load-id="IDENTIFIER"
+
+*/
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    $('[data-tam-load-element="load-target"]').each(function (i) {
+        var load_target = $(this);
+        var load_collection = load_target.attr('data-tam-load-collection');
+        var load_item = load_target.attr('data-tam-load-item');
+        var load_id = load_target.attr('data-tam-load-id');
+        load_target.load(
+            '/' +
+                load_collection +
+                '/' +
+                load_item +
+                ' ' +
+                "[data-tam-load-element='load-source'][data-tam-load-id='" +
+                load_id +
+                "']"
+        );
+    });
+});
